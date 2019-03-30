eb5fd2c7b7d7c44891f30e3b3b0c5396ee0aa967
asylum29 <ksenon3@mail.ru>
2019-03-30 23:44:18 +0700

    add registration for REST

--- src/Controller/Security/SecurityController.php
+++ src/Controller/Security/SecurityController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller\Security;
 
+use FOS\UserBundle\Model\UserManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends \FOS\UserBundle\Controller\SecurityController
 {
@@ -14,5 +19,56 @@ class SecurityController extends \FOS\UserBundle\Controller\SecurityController
         }
 
         return parent::loginAction($request);
+    }
+
+    /**
+     * @Route(
+     *     "/user/register",
+     *     name="user.register", methods={"POST"},
+     *     condition="request.headers.get('Content-Type') == 'application/json'"
+     * )
+     */
+    public function registerAction(
+        Request $request,
+        JWTTokenManagerInterface $jwtManager,
+        UserManagerInterface $userManager
+    )
+    {
+        $userData = json_decode($request->getContent(), true);
+
+        if (empty($userData['email'])) {
+            return new JsonResponse(['email' => 'required'], 400);
+        }
+
+        if (empty($userData['username'])) {
+            return new Response(['username' => 'required'], 400);
+        }
+
+        if (empty($userData['password'])) {
+            return new Response(['password' => 'required'], 400);
+        }
+
+        if ($userManager->findUserByEmail($userData['email'])) {
+            return new JsonResponse(['email' => 'unique'], 400);
+        }
+
+        if ($userManager->findUserByUsername($userData['username'])) {
+            return new JsonResponse(['username' => 'unique'], 400);
+        }
+
+        $user = $userManager->createUser();
+        $user->setEnabled(true);
+        $user->setEmail($userData['email']);
+        $user->setUsername($userData['username']);
+        $user->setPlainPassword($userData['password']);
+        $userManager->updateUser($user);
+
+        return new JsonResponse(
+            [
+                'id'    => $user->getId(),
+                'token' => $jwtManager->create($user)
+            ],
+            JsonResponse::HTTP_CREATED
+        );
     }
 }
