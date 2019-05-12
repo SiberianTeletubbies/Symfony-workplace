780498f014e2975241044143a73064720ce7b08d
asylum29 <ksenon3@mail.ru>
2019-05-12 22:25:38 +0700

    migration fix

--- src/Migrations/Version20190512140510.php
+++ src/Migrations/Version20190512140510.php
@@ -15,9 +15,6 @@ final class Version20190512140510 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526CE2904019');
-        $this->addSql('DROP TABLE comment');
-        $this->addSql('DROP TABLE thread');
         $this->addSql('ALTER TABLE task ADD image VARCHAR(1000) DEFAULT NULL, ADD image_file_name VARCHAR(1000) DEFAULT NULL');
     }
 
@@ -26,10 +23,6 @@ final class Version20190512140510 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, thread_id VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci, author_id INT DEFAULT NULL, body LONGTEXT NOT NULL COLLATE utf8mb4_unicode_ci, ancestors VARCHAR(1024) NOT NULL COLLATE utf8mb4_unicode_ci, depth INT NOT NULL, created_at DATETIME NOT NULL, state INT NOT NULL, INDEX IDX_9474526CF675F31B (author_id), INDEX IDX_9474526CE2904019 (thread_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('CREATE TABLE thread (id VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, permalink VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, is_commentable TINYINT(1) NOT NULL, num_comments INT NOT NULL, last_comment_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526CE2904019 FOREIGN KEY (thread_id) REFERENCES thread (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
-        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526CF675F31B FOREIGN KEY (author_id) REFERENCES fos_user (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
         $this->addSql('ALTER TABLE task DROP image, DROP image_file_name');
     }
 }
